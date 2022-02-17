d329f78f9491ac8f70367bc5caa6aeee1a978388
Sabbir Hossain <82939905+devlopersabbir@users.noreply.github.com>
2022-02-17 17:41:46 +0600

    Delete dark.js

--- dark.js
+++
@@ -1,8 +0,0 @@
-const bodyDarkColor = document.getElementsByTagName("body")[0];
-const darkMode = document.getElementById("darkMode");
-
-
-darkMode.addEventListener("click",()=>{
-    darkMode.classList.toggle("active");
-    bodyDarkColor.classList.toggle("night");
-},2000);
